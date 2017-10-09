613518a85c64b30aa27d7896b1f36da1e33be594
angel <githubUniversity@gmail.com>
2017-10-09 02:34:32 +0200

    Adaptación companyResourceTest a Dao, Resource y Controller

--- src/test/java/es/upm/miw/apaw/company/CompanyResourceFunctionalTesting.java
+++ src/test/java/es/upm/miw/apaw/company/CompanyResourceFunctionalTesting.java
@@ -2,8 +2,12 @@ package es.upm.miw.apaw.company;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import es.upm.miw.apaw.company.api.daos.DaoFactory;
+import es.upm.miw.apaw.company.api.daos.memory.DaoMemoryFactory;
+import es.upm.miw.apaw.company.api.resources.CompanyResource;
 import es.upm.miw.apaw.company.http.HttpClientService;
 import es.upm.miw.apaw.company.http.HttpException;
 import es.upm.miw.apaw.company.http.HttpMethod;
@@ -12,9 +16,13 @@ import es.upm.miw.apaw.company.http.HttpRequestBuilder;
 
 public class CompanyResourceFunctionalTesting {
 
+    @Before
+    public void before() {
+        DaoFactory.setFactory(new DaoMemoryFactory());
+    }
     
     private void createCompany() {
-        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path("companies").body("company1").build();
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(CompanyResource.COMPANIES).body("company1").build();
         new HttpClientService().httpRequest(request);
     }
 
@@ -25,27 +33,27 @@ public class CompanyResourceFunctionalTesting {
 
     @Test(expected = HttpException.class)
     public void testCreateCompanyNameEmpty() {
-        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path("companies").body("").build();
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(CompanyResource.COMPANIES).body("").build();
         new HttpClientService().httpRequest(request);
     }
 
     @Test(expected = HttpException.class)
     public void testCreateWithoutCompanyName() {
-        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path("companies").build();
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(CompanyResource.COMPANIES).build();
         new HttpClientService().httpRequest(request);
     }
 
     @Test
     public void testReadCompany() {
         this.createCompany();
-        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path("companies").path("/{id}")
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path(CompanyResource.COMPANIES).path(CompanyResource.ID)
                 .expandPath("1").build();
         assertEquals("{\"id\":1,\"name\":\"company1\"}", new HttpClientService().httpRequest(request).getBody());
     }
     
     @Test(expected = HttpException.class)
     public void testReadCompanyCompanyIdNotFound() {
-        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path("companies").path("/{id}")
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path(CompanyResource.COMPANIES).path(CompanyResource.ID)
                 .expandPath("2").build();
         new HttpClientService().httpRequest(request);
     }
@@ -53,21 +61,21 @@ public class CompanyResourceFunctionalTesting {
     @Test
     public void testCompanyList() {
         this.createCompany();
-        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path("companies").build();
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path(CompanyResource.COMPANIES).build();
         assertEquals("[{\"id\":1,\"name\":\"company1\"}]", new HttpClientService().httpRequest(request).getBody());
     }
     
     @Test
     public void testDeleteCompany() {
         this.createCompany();
-        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.DELETE).path("companies").path("/{id}")
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.DELETE).path(CompanyResource.COMPANIES).path(CompanyResource.ID)
                 .expandPath("1").build();
         new HttpClientService().httpRequest(request);
     }
     
     @Test(expected = HttpException.class)
     public void testDeleteCompanyCompanyIdNotFoundException() {
-        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.DELETE).path("companies").path("/{id}")
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.DELETE).path(CompanyResource.COMPANIES).path(CompanyResource.ID)
                 .expandPath("2").build();
         new HttpClientService().httpRequest(request);
     }
